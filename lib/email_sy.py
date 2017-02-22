17a1725b73218e68de3e5f6503f21bb9da180206
wyh <wannianma@foxmail.com>
2017-02-22 16:56:40 +0800

    Fix 发送邮件模块

--- lib/email_sy.py
+++ lib/email_sy.py
@@ -1,5 +1,5 @@
 #-*-coding:utf-8-*-
-#!/usr/bin/python
+#!/usr/bin/python3.5
 
 #
 # Send Email with Content Log
@@ -11,69 +11,79 @@ from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-import smtplib
-import config
-import queue_sy
 from datetime import datetime
-import json
-import time
-
-email_queue = queue_sy.Queue()
-
-def send_email(subject, content):
-    # 判断邮件发送队列是否超过最大发送数目
-    if email_queue.size() <= config.EMAIL_MAX_SIZE:
-        # 获取当前时间 
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S');
-        email_queue.lpush('{"time":"%s", "subject":"%s", "content":"%s"}' % (current_time, subject, content))
-        return True
-    else:
-        return False
+from queue_sy import Sy_Queue
+from log_sy import logger
+import smtplib, json, time, atexit
+import config
 
 
-def _format_addr(s, charset):
-    name, addr = parseaddr(s)
-    return formataddr(( \
-        Header(name, charset).encode(), \
-        addr.encode(charset) if isinstance(addr, unicode) else addr))
+class Sy_Email(object):
+    ## Class Attribute, 类变量
+    name = 'Email'
 
-def _send_email(subject, content):
-    from_addr = config.EMAIL_FROM
-    password = config.EMAIL_PASSWORD
-    to_addr = config.EMAIL_TO
-    smtp_server = config.EMAIL_SMTP_SERVER
+    def __init__(self):
+        ## private variable begin with '__'
+        self.__email_queue = Sy_Queue()
+        self.__from_addr = config.EMAIL_FROM
+        self.__to_addr = config.EMAIL_TO
+        self.__charset = config.EMAIL_CHARSET
+        # 设置发送服务器    
+        self.__server = smtplib.SMTP(config.EMAIL_SMTP_SERVER, config.EMAIL_PORT)
+        try:
+            self.__server.set_debuglevel(0)
+            self.__server.login(self.__from_addr, config.EMAIL_PASSWORD)
+        except Exception as e:
+            logger.error("EMAIL SERVER INIT FAIL, info:{0}".format(e))
+            self.__server = None
+        finally:
+            # 注册自动释放连接函数
+            atexit.register(self.destroy)
 
-    msg = MIMEText(content, 'plain', config.EMAIL_CHARSET)
-    msg['From'] = _format_addr(u'Shenyou.TV<%s>' % from_addr, config.EMAIL_CHARSET)
-    msg['To'] = _format_addr(u'管理员<%s>' % to_addr, config.EMAIL_CHARSET)
-    msg['subject'] = Header(subject, config.EMAIL_CHARSET).encode()
+    def destroy(self):
+        logger.info("SMTP Server close!")
+        self.__server.quit()
 
-    server = smtplib.SMTP(smtp_server, config.EMAIL_PORT)
-    server.set_debuglevel(0)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, [to_addr], msg.as_string())
-    server.quit()
+    # 发送email方法
+    def send_email(self, subject, content, to_addr=None, content_type='plain'):
+        to_addr = to_addr if to_addr else self.__to_addr
+        msg = MIMEText(content, content_type, self.__charset)
+        msg['From'] = self._format_addr('ShenYou.TV<{0}>'.format(self.__from_addr))
+        msg['To'] = self._format_addr('管理员<{0}>'.format(to_addr))
+        msg['subject'] = Header(subject, self.__charset).encode()
 
-def send_emails():
-    # 每次发送15封邮件,如果邮件列表只包含一封邮件，不发送
-    emails = email_queue.getjobs(15)
-    while emails and len(emails) > 3:
-        email_content = ""
-        email_suject = u"爬取日志"
+        if not self.__server is None:
+            self.__server.sendmail(self.__from_addr, [self.__to_addr], msg.as_string())
+            logger.info("send email success, from:{0}, to:{1}".format(self.__from_addr, self.__to_addr))
+
+    def _format_addr(self, s):
+        name, addr = parseaddr(s)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+
+    
+    # push到邮件发送队列
+    def push_email(self, subject, content, to_addr=None):
+        to_addr = to_addr if to_addr else self.__to_addr
+        self.__email_queue.lpush('{"to_addr":"%s", "subject":"%s", "content":"%s"}'% (to_addr, subject, content))
+    
+    # 批量发送邮件
+    def send_emails(self):
+        emails = self.__email_queue.getjobs()
         for em in emails:
             obj = json.loads(em)
-            current_time = obj['time']
             subject = obj['subject']
-            content = obj['content']
-            email_content = email_content + current_time + "," + subject + " : " + content + "\r\n"
-        _send_email(email_suject, email_content)
-        # 防止不间断发送邮件遭拒收
-        time.sleep(2)
-        emails = email_queue.getjobs(15)
+            content= obj['content']
+            address = obj['to_addr']
+            self.send_email(subject, content, to_addr = address)
+            # 防止不间断发送邮件遭拒收
+            time.sleep(2)
 
 def run():
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S');
-    send_email(u'测试发送邮件模块', u'当前时间' + current_time)
+    email = Sy_Email()
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S');
+    for i in range(1):
+        email.push_email('测试发送邮件模块', '当前时间' + current_time)
+    email.send_emails()
 
 
 if __name__ == "__main__":
